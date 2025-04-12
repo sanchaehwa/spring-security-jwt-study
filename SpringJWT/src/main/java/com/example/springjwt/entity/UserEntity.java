3903b0fc82e3f09a1130a899fb0932e96263950f
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-04-13 01:50:35 +0900

    JWT 5까지 실습 파일 업로드

---
+++ SpringJWT/src/main/java/com/example/springjwt/entity/UserEntity.java
@@ -0,0 +1,18 @@
+package com.example.springjwt.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+@Entity
+@Setter
+@Getter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+    private String password;
+
+    private String role;
+}
